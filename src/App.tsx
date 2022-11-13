215cb8d1fcd0f05158c4db4adb98192851aad155
Rick Boykin <rick.boykin@gmail.com>
2022-11-12 19:49:58 -1000

    updates to use tailwindcss-container-query

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="test-container">
-      <div className="test-child">this is a test underlined at 36rem</div>
-    </div>
+    <>
+      <div className="test-container">
+        <div className="test-child">this is a test underlined at 36rem</div>
+      </div>
+      <div className="container-type-inline-size">
+        <div className="cq-w-22:underline">
+          this is a test underlined at 36rem
+        </div>
+      </div>
+    </>
   );
 }
 
